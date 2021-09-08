d17deb8caad43a1422b12afd949628913cfcf8bc
Alexander Korkin <A.A.Korkin@urfu.me>
2021-09-08 23:56:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 try:
     number_one = float(input('Введите первое число: '))
     number_two = float(input('Введите второе число: '))
-
+    #Ввод чисел
+    
 except ValueError:
+
+    """Если введен текст, то программа вместо ошибки выдаст,
+    что введены некорректные данные"""
+
     print('\nВведены некорректные данные')
     input('Press ENTER to exit')
-
+    
 else:
-    number_one = float(number_one)
-    number_two = float(number_two)
-    Sum = number_one + number_two
-    Diff = number_one - number_two
-    Ave = (abs(number_one) + abs(number_two)) / 2
+    Sum = number_one + number_two   #Сумма
+    Diff = number_one - number_two  #Разность
+    Ave = (abs(number_one) + abs(number_two)) / 2   #Среднее арифмитическое
 
     print('\nСумма этих чисел равна {0}\nРазность этих чисел равна {1}\n'
           'Среднее арифмитическое этих чисел равно {2}'.format(Sum, Diff, Ave))
+    #Вывод
     input('Press ENTER to exit')
