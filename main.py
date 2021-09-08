7452326756c23c46f542da4e03a379627a342195
Alexander Korkin <A.A.Korkin@urfu.me>
2021-09-09 01:03:37 +0500

    Update main.py
    Добавлены:
    Вычисление и вывод произведения чисел и частного от их деления

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 try:
     number_one = float(input('Введите первое число: '))
     number_two = float(input('Введите второе число: '))
-    #Ввод чисел
+    # Ввод чисел
     
 except ValueError:
 
-    """Если введен текст, то программа вместо ошибки выдаст,
-    что введены некорректные данные"""
+    # Если введен текст, то программа вместо ошибки выдаст,
+    # что введены некорректные данные
 
     print('\nВведены некорректные данные')
     input('Press ENTER to exit')
     
 else:
-    Sum = number_one + number_two   #Сумма
-    Diff = number_one - number_two  #Разность
-    Ave = (abs(number_one) + abs(number_two)) / 2   #Среднее арифмитическое
+    Sum = number_one + number_two   # Сумма
+    Diff = number_one - number_two  # Разность
+    Ave = (abs(number_one) + abs(number_two)) / 2   # Среднее арифмитическое
+    Prod = number_one * number_two  # Произведенеие
+    Quot = number_one / number_two  # Частное
 
-    print('\nСумма этих чисел равна {0}\nРазность этих чисел равна {1}\n'
-          'Среднее арифмитическое этих чисел равно {2}'.format(Sum, Diff, Ave))
-    #Вывод
+    print('\nСумма этих чисел равна {}\n'
+          'Разность этих чисел равна {}\n'
+          'Среднее арифмитическое этих чисел равно {}\n'
+          'Произведение этих чисел равно {}\n'
+          'Частное от деления этич чисел равно {}'.format(Sum, Diff, Ave, Prod, Quot))
+    # Вывод
     input('Press ENTER to exit')
